22dc071d7cc2d11611815756b2dabe210dd63a7e
redream123 <redream123@126.com>
2018-11-20 16:56:09 +0800

    181120

--- prog/python/autoStudy_CMA/autoStudy_CMA.py
+++ prog/python/autoStudy_CMA/autoStudy_CMA.py
@@ -1,10 +1,33 @@
-import os
+# -*- encoding: utf-8 -*-
 
-# kdfkjl
-for i in (1, 2, 3):
-    print(i)
-# dkjf
-# sdfljlsfj
-for x in (3, 4, 6):
-    i = x
-# kdfk
+"""
+@File    :   autoStudy_CMA.py
+@Time    :   2018/11/18 23:54:44
+@Author  :   YWB
+@Version :   1.0
+@Contact :   xxx@xxx.com
+@License :   (C)Copyright 2017-2018
+@Desc    :   None
+
+"""
+
+# here put the import lib
+from selenium import webdriver as wd
+
+cmatc_url = "http://www.cmatc.cn/lms"
+iedriver = r"C:\Program Files\Internet Explorer\IEDriverServer32.exe"
+
+drv = wd.Ie(iedriver)
+drv.get(cmatc_url)
+drv.implicitly_wait(10)
+drv.find_element_by_id("username").send_keys("770753")
+drv.find_element_by_id("password").send_keys("123456")
+drv.find_element_by_name("submit").click()
+drv.get(
+    "http://www.cmatc.cn/lms/app/lms/student/Userselectlesson/show.do?lessonId=112427"
+)
+# drv.find_element_by_css_selector("span.btn btn-success").click()
+# drv.find_element_by_class_name("btn btn-success")
+# 上述两种方式均未找到相应的span元素，未知原因。
+drv.find_element_by_xpath('//span[@class="btn btn-success"]').click()
+
